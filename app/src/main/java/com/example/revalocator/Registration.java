0c82e063ad8e7ffc1ef2e20abcd22e037fe3ba5a
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2024-05-04 18:45:43 +0530

    Registration UI Done

---
+++ app/src/main/java/com/example/revalocator/Registration.java
@@ -0,0 +1,15 @@
+package com.example.revalocator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+    }
+}
