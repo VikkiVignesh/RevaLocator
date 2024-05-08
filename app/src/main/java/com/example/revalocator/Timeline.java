8796c223786fcc01d48d0fce803a4aa1663d588d
pintaram <pintu.borana20@gmail.com>
2024-05-08 17:48:36 +0530

    Time line update

--- app/src/main/java/com/example/revalocator/Timeline.java
+++ app/src/main/java/com/example/revalocator/Timeline.java
@@ -1,6 +1,7 @@
 package com.example.revalocator;
 
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +12,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.firebase.database.DataSnapshot;
@@ -31,8 +40,12 @@ public class Timeline extends Fragment implements OnMapReadyCallback {
 
     private DatabaseReference mDatabase;
     private GoogleMap mMap;
-    private double defaultLat = 13.114750;
-    private double defaultLng = 77.634684;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
+    private double startLat = 12.974148;
+    private double startLng = 77.561220;
+    private List<LatLng> defaultCoordinates = new ArrayList<>();
+    private Polyline polyline;
 
     @Nullable
     @Override
@@ -50,33 +63,50 @@ public class Timeline extends Fragment implements OnMapReadyCallback {
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
+
+        // Add default coordinates to the list
+        defaultCoordinates.add(new LatLng(12.974470, 77.561125));
+        defaultCoordinates.add(new LatLng(12.973643, 77.561067));
+
+        // Initialize fusedLocationClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext());
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        drawGraph(defaultLat, defaultLng, 12.974148,77.561220);
+
         // Check if mMap is null before using it
         if (mMap != null) {
+            // Add markers for default coordinates
+            for (LatLng coordinate : defaultCoordinates) {
+                mMap.addMarker(new MarkerOptions().position(coordinate));
+            }
+
+            // Start location updates
+            startLocationUpdates();
+
             // Listener to retrieve data from the database
             mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                     try {
-                        // Loop through all marker locations
+                        // Draw polyline for the path
+                        List<LatLng> pathCoordinates = new ArrayList<>();
+                        pathCoordinates.add(new LatLng(startLat, startLng));
+
                         for (DataSnapshot markerSnapshot : dataSnapshot.getChildren()) {
                             Double latitude = markerSnapshot.child("latitude").getValue(Double.class);
                             Double longitude = markerSnapshot.child("longitude").getValue(Double.class);
 
                             // Check if latitude and longitude are not null
                             if (latitude != null && longitude != null) {
-                                // Check if the retrieved coordinates match the default coordinates
-                                if (latitude.equals(defaultLat) && longitude.equals(defaultLng)) {
-                                    // Coordinates match, draw a graph from default location to this location
-                                    //drawGraph(defaultLat, defaultLng, latitude, longitude);
-                                }
+                                LatLng currentCoordinate = new LatLng(latitude, longitude);
+                                pathCoordinates.add(currentCoordinate);
                             }
                         }
+
+                        drawPath(pathCoordinates);
                     } catch (Exception e) {
                         // Handle any exceptions that occur during data retrieval
                         e.printStackTrace();
@@ -95,28 +125,82 @@ public class Timeline extends Fragment implements OnMapReadyCallback {
         }
     }
 
-    private void drawGraph(double startLat, double startLng, double endLat, double endLng) {
+    private void startLocationUpdates() {
+        // Create the location request to start receiving updates
+        LocationRequest locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10000) // 10 seconds
+                .setFastestInterval(5000); // 5 seconds
+
+        // Create location callback
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                if (locationResult != null) {
+                    Location location = locationResult.getLastLocation();
+                    if (location != null) {
+                        updateLocation(location);
+                    }
+                }
+            }
+        };
+
+        // Request location updates
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+    }
+
+    private void updateLocation(Location location) {
+        // Check if user's location is near any of the default points
+        for (LatLng coordinate : defaultCoordinates) {
+            float[] distance = new float[1];
+            Location.distanceBetween(location.getLatitude(), location.getLongitude(),
+                    coordinate.latitude, coordinate.longitude, distance);
+            if (distance[0] < 3) { // Adjust this value as needed for your accuracy requirements
+                // Do something when user is near the coordinate
+                mMap.addMarker(new MarkerOptions().position(coordinate).icon(getMarkerIcon(Color.GREEN)));
+            }
+        }
+    }
+
+    private void drawPath(List<LatLng> pathCoordinates) {
         try {
             // Check if mMap is null before using it
             if (mMap != null) {
-                List<LatLng> points = new ArrayList<>();
-                points.add(new LatLng(startLat, startLng));
-                points.add(new LatLng(endLat, endLng));
-
+                // Draw polyline for the path
                 PolylineOptions polylineOptions = new PolylineOptions()
-                        .addAll(points)
+                        .addAll(pathCoordinates)
                         .color(Color.RED)
                         .width(5);
 
-                Polyline polyline = mMap.addPolyline(polylineOptions);
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(startLat, startLng), 12));
+                polyline = mMap.addPolyline(polylineOptions);
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pathCoordinates.get(0), 12));
             } else {
                 // Handle the case where mMap is null
                 Toast.makeText(getContext(), "Map is not available", Toast.LENGTH_SHORT).show();
             }
         } catch (Exception e) {
-            // Handle any exceptions that occur during graph drawing
+            // Handle any exceptions that occur during polyline drawing
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Stop location updates when the fragment is destroyed
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        if (fusedLocationClient != null && locationCallback != null) {
+            fusedLocationClient.removeLocationUpdates(locationCallback);
+        }
+    }
+
+    private BitmapDescriptor getMarkerIcon(int color) {
+        float[] hsv = new float[3];
+        Color.colorToHSV(color, hsv);
+        return BitmapDescriptorFactory.defaultMarker(hsv[0]);
     }
 }
